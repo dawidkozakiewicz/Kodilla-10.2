b38ae777bcc43b1b5be90632cb22f104b7768dbc
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-12-24 22:05:23 +0100

    better understanding play

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
 ﻿var span = $("span");
-span.each(function(index, element) {});
+// span.each(function(index, element) {});
 
-span.each(function(index, element) {
+span.each(function (index, boo) {
   if (index % 2 == 0) {
-    $(element).css('color', 'red');
+    $(boo).css('color', '#fff900');
   };
 });
 
 var paragraphs = $('p');
-paragraphs.each(function(index, element) {
+paragraphs.each(function (index, foo) {
 
-  var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-  $(element).append(button)
+  var button = '<button class="btn" cokolwiek="' + index + '">Click me</button>';
+  $(foo).append(button);
 
 });
 
-$("button").click(function() {
-  alert($(this).attr("data-tmp"));
+$('button').click(function () {
+  alert($(this).attr("cokolwiek"));
 });
+
+
